754a1bc6d4639ad3db3d5e0e9662268ff8e885ea
amit <amit.jha@candere.com>
2023-08-22 17:57:27 +0530

    user sectoin added

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -3,9 +3,8 @@ import './Header.scss';
 import Menu from '../menu/Menu';
 import Search from '../search/Search';
 import { Link } from 'react-router-dom';
-import { IconContext } from "react-icons";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { FiMapPin , FiHeadphones, FiUser, FiHeart} from "react-icons/fi";
+import { FiMapPin , FiHeadphones, FiUser, FiHeart, FiShoppingBag,FiChevronRight} from "react-icons/fi";
 
 const HeaderTop = ()=>{
   return(
@@ -30,56 +29,98 @@ const HeaderMiddle=()=>{
         </div>     
         <div className="item_right">
           <div className="offers_contact">
-              <div className="icon_container">
+              <Link className="icon_container" to="/">
                 <span className="icon">
                   <img src={process.env.PUBLIC_URL + '/images/icons/discount.svg'} />                 
                 </span>
                 <span className="title">Offers</span>
-              </div>
-              <div className="icon_container">
+              </Link>
+              <Link className="icon_container" to="/">
                   <span className="icon_wrapper"> 
                     <FiMapPin />
                   </span>  
                 <span className="title">Find a Store</span>            
-              </div>
-              <div className="icon_container">
+              </Link>
+              <Link className="icon_container" to="/">
                 <span className="icon_wrapper"> 
                   <FiHeadphones />
                 </span>
                 <span className="title">Contact Us</span>
-              </div>
+              </Link>
           </div>
           <div className="profile_cart">
-            <div className="icon_container">
+              <div className="icon_container profile">
                 <span className="icon_wrapper"> 
                   <FiUser />
                 </span>
                 <span className="title">Profile</span>
+                <div className="profile_container">                  
+                  <div className="login_container">
+                    <div className="title_container">
+                        <h4>Welcome</h4>
+                        <p>To access account and manage orders</p>
+                    </div>
+                    <Link className="btn login_btn" to="/">
+                      <span>Login to your account</span>
+                    </Link>
+                  </div>
+                  <Link className="profile_item_container" to="/">
+                    <div className="icon_container">
+                      <span className="icon_wrapper"> 
+                        <FiUser />
+                      </span>
+                      <span className="small_text">Personal Information</span>
+                    </div>
+                    <div class="angle_right">
+                      <FiChevronRight />
+                    </div>
+                  </Link>
+                  <Link className="profile_item_container" to="/">
+                    <div className="icon_container">
+                      <span className="icon_wrapper"> 
+                        <FiShoppingBag />
+                      </span>
+                      <span className="small_text">My Order</span>
+                    </div>
+                    <div class="angle_right">
+                      <FiChevronRight />
+                    </div>
+                  </Link>
+                  <Link className="profile_item_container" to="/">
+                    <div className="icon_container">
+                      <span className="icon_wrapper"> 
+                        <FiHeart />
+                      </span>
+                      <span className="small_text">My Wishlist</span>
+                    </div>
+                    <div class="angle_right">
+                      <FiChevronRight />
+                    </div>
+                  </Link>
+                </div>
               </div>
-              <div className="icon_container relative_icon">
+              <Link className="icon_container relative_icon" to="/">
                 <span className="icon_wrapper"> 
                   <FiHeart />
                 </span>
                 <span className="title">Wishlist</span>
                 <span className="total_item">1</span>
-              </div>
-              <div className="icon_container relative_icon">
+              </Link>
+              <Link className="icon_container relative_icon" to="/">
                 <span className="icon_wrapper"> 
                   <AiOutlineShoppingCart />
                 </span>
                 <span className="total_item">1</span>
                 <span className="title">Cart</span>
-              </div> 
+              </Link> 
           </div>
           <div className="express">
-              <div className="icon_container">
-                <div className="icon_container">
+                <Link className="icon_container" to="/">
                   <span className="icon">
                     <img src={process.env.PUBLIC_URL + '/images/icons/express.svg'} />            
                   </span>
                   <span className="title">Express</span>
-                </div>
-              </div>
+                </Link>
           </div>
         </div>        
       </div>
